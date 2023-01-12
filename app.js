b68c2a8981293b950091fb90152ecd61f621355f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-12 17:29:49 -0500

    feat: add routes for sessions

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+
+app.get('/', (req, res) => {
+    req.session.user = {
+        username: 'alex',
+        role: 'admin'
+    }
+    res.send({ message: 'ok' })
+})
+
+app.get('/currentUser', (req, res) => {
+    res.send(req.session.user)
+})
+
+
+
